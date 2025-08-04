2834a2b984bb37df72780d84ed98676882a73f87
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-08-04 22:29:27 +0530

    DeleteDev method implementation

--- src/main/java/com/company/serviceImpl/DeveloperServiceImpl.java
+++ src/main/java/com/company/serviceImpl/DeveloperServiceImpl.java
@@ -31,4 +31,31 @@ public class DeveloperServiceImpl implements DeveloperService {
         Developer developer =  developerRepository.findById(id).orElseThrow(() -> new NullPointerException("Developer with id not found" +id));
         return developer;
     }
+
+    @Override
+    public String deleteDev(int id) {
+        developerRepository.deleteById(id);
+        return  "Developer deleted";
+    }
+
+    @Override
+    public Developer updateDev(int id, Developer newData) {
+
+        Developer developer = developerRepository.findById(id).orElseThrow(()->new NullPointerException(("Data not found"+ id)));
+
+        developer.setFname(newData.getFname());
+        developer.setAge(newData.getAge());
+        developer.setCity(newData.getCity());
+        developer.setLname(newData.getLname());
+        developer.setSalary(newData.getSalary());
+
+        Developer updatedDev = developerRepository.save(developer);
+        return  updatedDev;
+    }
+
+    @Override
+    public String saveListDev(List<Developer> developers) {
+        List<Developer> developerList = developerRepository.saveAll(developers);
+        return "List saved";
+    }
 }
