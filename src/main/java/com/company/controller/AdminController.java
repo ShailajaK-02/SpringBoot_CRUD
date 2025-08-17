c663708fa4d9b1be25c1e8dee1c0ed4f55b78960
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-08-17 12:34:25 +0530

    Admin controller added

---
+++ src/main/java/com/company/controller/AdminController.java
@@ -0,0 +1,30 @@
+package com.company.controller;
+
+import com.company.entity.Admin;
+import com.company.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    //inject service in this
+    @Autowired
+    private AdminService adminService;
+
+    //post list of admin indatabase
+    @PostMapping("/addAdmin")
+    public ResponseEntity<String> addAdmin(@RequestBody List<Admin> admins){
+        adminService.saveAdmin(admins);
+        return new ResponseEntity<>("Admin added", HttpStatus.OK);
+    }
+
+}
