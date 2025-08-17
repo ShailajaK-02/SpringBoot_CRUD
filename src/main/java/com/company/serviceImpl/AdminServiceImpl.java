c3ffa791ac9de64a10f640781e44924a210bb704
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-08-17 12:48:12 +0530

    delete method implemented

--- src/main/java/com/company/serviceImpl/AdminServiceImpl.java
+++ src/main/java/com/company/serviceImpl/AdminServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AdminServiceImpl implements AdminService {
@@ -25,5 +26,11 @@ public class AdminServiceImpl implements AdminService {
     public List<Admin> getallAdminData() {
         List<Admin> adminList = repository.findAll();
         return adminList;
+    }
+
+    @Override
+    public String deleteAdmin(int id) {
+        repository.deleteById(id);
+        return "Admin deleted";
     }
 }
