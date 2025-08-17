650cd827645142fcaaac000fd72aca54b3ca075e
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-08-17 12:34:55 +0530

    Admin service added

---
+++ src/main/java/com/company/service/AdminService.java
@@ -0,0 +1,13 @@
+package com.company.service;
+
+import com.company.entity.Admin;
+
+import java.util.List;
+
+public interface AdminService {
+
+    //add admin
+    String saveAdmin(List<Admin> admins);
+
+
+}
