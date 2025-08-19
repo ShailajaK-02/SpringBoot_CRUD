937ec1cb87877583f884c25948e8ae45a797372c
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-08-19 21:07:40 +0530

    Custom exception added

--- src/main/java/com/company/exception/DeveloperNotFoundException.java
+++ src/main/java/com/company/exception/DeveloperNotFoundException.java
@@ -1,5 +1,6 @@
 package com.company.exception;
 
+//This is custome exception
 public class DeveloperNotFoundException extends RuntimeException
 {
     public DeveloperNotFoundException(String message) {
