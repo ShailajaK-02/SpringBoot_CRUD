70b1514be1dbe7edce947035358cf1805cd629a1
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-08-21 00:27:11 +0530

    JPQL commit

--- src/main/java/com/company/exception/DeveloperNotFoundException.java
+++ src/main/java/com/company/exception/DeveloperNotFoundException.java
@@ -1,9 +1,9 @@
-package com.company.exception;
-
-//This is custome exception
-public class DeveloperNotFoundException extends RuntimeException
-{
-    public DeveloperNotFoundException(String message) {
-        super(message);
-    }
-}
+//package com.company.exception;
+//
+////This is custome exception
+//public class DeveloperNotFoundException extends RuntimeException
+//{
+//    public DeveloperNotFoundException(String message) {
+//        super(message);
+//    }
+//}
