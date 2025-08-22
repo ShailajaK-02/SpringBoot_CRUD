475d5d3c61dd3c65fe1295007c98390122cfebb3
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-08-22 21:13:30 +0530

    Schedular added

--- src/main/java/com/company/GlobalExceptionHandler.java
+++ src/main/java/com/company/GlobalExceptionHandler.java
@@ -1,17 +1,23 @@
-//package com.company;
-//
-//import com.company.exception.DeveloperNotFoundException;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.ExceptionHandler;
-//import org.springframework.web.bind.annotation.RestControllerAdvice;
-//
-////To handle the global exceptions
-//@RestControllerAdvice
-//public class GlobalExceptionHandler {
-//
-//    @ExceptionHandler(DeveloperNotFoundException.class)
-//    public ResponseEntity<String> handleDevNotFoundException(DeveloperNotFoundException exception){
-//        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+package com.company;
+
+import com.company.exception.DeveloperNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+//To handle the global exceptions
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+//    @ExceptionHandler(Exception.class)
+//    public ResponseEntity<String> handleAll(Exception ex) {
+//        ex.printStackTrace(); // optional: log it
+//        return ResponseEntity.internalServerError()
+//                .body("Something went wrong.");
 //    }
-//}
+    @ExceptionHandler(DeveloperNotFoundException.class)
+    public ResponseEntity<String> handleDevNotFoundException(DeveloperNotFoundException exception){
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
